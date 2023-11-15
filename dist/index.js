786efedb9f4ebdae7da6b47b54f4c13ab2b56fcc
Aaron Horner <ahorner@centeredgesoftware.com>
2023-11-15 15:42:39 -0500

    Fix newlines in merged markdown

--- dist/index.js
+++ dist/index.js
@@ -2918,12 +2918,13 @@ async function mergeCode(filePath) {
             if (replacement) {
                 for (const l of replacement) {
                     ws.write(l);
+                    ws.write('\r\n');
                 }
                 continue;
             }
-            core.debug(`final line: ${line}`);
         }
         ws.write(line);
+        ws.write('\r\n');
     }
     ws.end();
     try {
